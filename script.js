cffbb792e0d943e13a0ea07b417f24aeb756f275
BaQuna <imbaquna@gmail.com>
2023-07-11 20:46:24 +0600

    hw js2

---
+++ script.js
@@ -0,0 +1,88 @@
+
+// let a='hello'
+// let b='Ivan'
+// let c=a[0]. toUpperCase()
+// let d=a.slice(1)
+// let e=b[0]. toLowerCase()
+// let f=b.slice(1)
+//
+// console.log(c+d+' '+e+f)
+
+
+// let a='lorem ipsum dolor'
+// let b=a.length
+// console.log(b)
+
+
+// let a='lorem ipsum dolor sit amet'
+// let b=a.slice(18,21)
+// let c=a.slice(0,18)
+// let d=a.slice(22,26)
+// console.log(b+' '+c+d)
+
+
+
+// let a='children'
+// let b=a.slice(7,8)
+// console.log(a.slice(0,7).toUpperCase()+b)
+
+
+
+// let a='boss'
+// let b=a.slice(0,1)
+// let c=a.slice(1,2)
+// let d=a.slice(2)
+//
+// console.log(b+d+c)
+
+
+
+// let a='sunday'
+// console.log(a.indexOf('d'))
+
+
+// let a='hello'
+// let b=a.slice(0,2)
+// let c='kso'
+// console.log(b+c)
+
+
+// let a='user'
+// let b='new'
+// let c=a.slice(0,2)
+//
+// console.log(b+' '+c)
+
+//
+// let a='monday'
+// let b='day'
+// let c=a.slice(0,3)
+// let d=a.slice(3,6)
+// console.log(b+d+c)
+
+
+// let a='HELLO'
+// let b=a[0].toLowerCase()
+// let c=a.slice(1,2)
+// let d=a[2].toLowerCase()
+// let f=a.slice(3,4)
+// let g=a[4].toLowerCase()
+//
+// console.log(b+c+d+f+g)
+
+
+// let a=5
+// let b='400'
+// let c=b.slice(0,1)
+// let d=b.slice(2,3)
+// console.log(c+a+d)
+
+
+let a='monkey'
+let b='banana'
+let c=a.slice(0,1)
+let d=b.slice(1,4)
+let f=a.slice(3,4)
+console.log(c+d+f)
+
+
